c00f41f20323cece2d0f6e75c7477b838f947bd2
Amin Mousavi <seyedamin963@gmail.com>
2024-03-18 15:00:47 +0330

    Build navbar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {}
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
 export default App;
